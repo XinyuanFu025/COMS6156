77d1544bd22b12676c66bcc90420510877ebbecc
XinyuanFu025 <63285781+XinyuanFu025@users.noreply.github.com>
2023-11-24 03:11:29 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,73 +1,54 @@
-from flask import Flask, redirect, request, session, url_for
-import requests
-from urllib.parse import urlencode
+from flask import Flask, redirect, url_for, session
+from flask_oauthlib.client import OAuth
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-app.config['GOOGLE_CLIENT_ID'] = '671071079747-1er03q01u8nab6v7o7oq81ao591ms4gl.apps.googleusercontent.com'
-app.config['GOOGLE_CLIENT_SECRET'] = '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'
-app.config['GOOGLE_REDIRECT_URI'] = 'http://localhost:8000/callback'
-#app.config['GOOGLE_REDIRECT_URI'] = 'http://34.16.183.53.nip.io:8000/callback'
-app.config['GOOGLE_AUTH_URL'] = 'https://accounts.google.com/o/oauth2/auth'
-app.config['GOOGLE_TOKEN_URL'] = 'https://accounts.google.com/o/oauth2/token'
-app.config['GOOGLE_USER_INFO_URL'] = 'https://www.googleapis.com/oauth2/v1/userinfo'
+app.secret_key = 'your_secret_key'  # Change this to a secure secret key
+
+oauth = OAuth(app)
+
+google = oauth.remote_app(
+    'google',
+    consumer_key='671071079747-1er03q01u8nab6v7o7oq81ao591ms4gl.apps.googleusercontent.com',
+    consumer_secret='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',
+    request_token_params={
+        'scope': 'email',
+    },
+    base_url='https://www.googleapis.com/plus/v1/',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+)
 
 @app.route('/')
-def home():
-    if 'google_token' in session:
-        user_info = get_user_info(session['google_token'])
-
-        # Check if 'name' key exists in user_info
-        if 'name' in user_info:
-            username = user_info['name']
-        else:
-            username = 'User'
-
-        # Display user information
-        return f'Hello, {username}! <a href="/logout">Logout</a>'
-    else:
-        return '<a href="/login">Login with Google</a>'
+def index():
+    return 'Welcome to the Flask SSO Example. <a href="/login">Login with Google</a>'
 
 @app.route('/login')
 def login():
-    return redirect(get_auth_url())
+    return google.authorize(callback=url_for('authorized', _external=True))
 
 @app.route('/logout')
 def logout():
     session.pop('google_token', None)
-    return redirect(url_for('home'))
+    return 'Logged out successfully.'
 
-@app.route('/callback')
-def callback():
-    code = request.args.get('code')
-    token = get_access_token(code)
-    session['google_token'] = token
-    return redirect(url_for('home'))
+@app.route('/login/authorized')
+def authorized():
+    response = google.authorized_response()
+    if response is None or response.get('access_token') is None:
+        return 'Access denied: reason={} error={}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    
+    session['google_token'] = (response['access_token'], '')
+    user_info = google.get('people/me')
+    return 'Logged in as: ' + user_info.data['displayName']
 
-def get_auth_url():
-    params = {
-        'client_id': app.config['GOOGLE_CLIENT_ID'],
-        'redirect_uri': app.config['GOOGLE_REDIRECT_URI'],
-        'scope': 'openid profile email',
-        'response_type': 'code',
-    }
-    return f"{app.config['GOOGLE_AUTH_URL']}?{urlencode(params)}"
-
-def get_access_token(code):
-    data = {
-        'code': code,
-        'client_id': app.config['GOOGLE_CLIENT_ID'],
-        'client_secret': app.config['GOOGLE_CLIENT_SECRET'],
-        'redirect_uri': app.config['GOOGLE_REDIRECT_URI'],
-        'grant_type': 'authorization_code',
-    }
-    response = requests.post(app.config['GOOGLE_TOKEN_URL'], data=data)
-    return response.json().get('access_token')
-
-def get_user_info(token):
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(app.config['GOOGLE_USER_INFO_URL'], headers=headers)
-    return response.json()
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
